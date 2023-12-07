84eaebb6b381864d6a4c84f622a1f37c2990ea5f
hlsylmz <halisyilmaz@gmail.com>
2023-12-07 15:58:31 +0100

    thirdcommit

---
+++ src/test/java/Tests/LedigaJobbClassTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import Pages.LedigaJobbClass;
+import TestBase.TestBase;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LedigaJobbClassTest extends TestBase {
+
+    @Test
+    void  ledigJobbTest() {
+        LedigaJobbClass jobb = new LedigaJobbClass(page);
+        jobb.ledigaJobb();
+        assertThat(page).hasURL("https://www.biltema.se/om-biltema/jobba-pa-biltema/lediga-tjanster/");
+
+    }
+}
---
+++ src/test/java/Tests/SearchArtikelPageTest.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import Pages.SearchArtikelPage;
+import TestBase.TestBase;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class SearchArtikelPageTest extends TestBase {
+    String searchItems="P-skiva";
+    @ Test
+    void searchTest() {
+        SearchArtikelPage searchArtikel= new SearchArtikelPage(page);
+        searchArtikel.searchArtikel(searchItems);
+        assertThat(page).hasURL(Pattern.compile(".*/?query="+ searchItems));
+    }
+}
