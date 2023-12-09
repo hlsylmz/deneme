ac11652e669ab5e513c49c557bfb409c8fbd58e6
hlsylmz <halisyilmaz@gmail.com>
2023-12-09 21:13:02 +0100

    kdk

--- src/test/java/TestBase/TestBase.java
+++ src/test/java/TestBase/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
     @BeforeAll
     static public void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5000));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500));
         //browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5000));
         //browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5000));
     }
