4e48cdd4b1cbdf3f55be331c843e41a6d87fcfb5
hlsylmz <halisyilmaz@gmail.com>
2024-02-05 21:57:38 +0100

    SearcArtikelPageTest updated

--- src/test/java/Pages/SearchArtikelPage.java
+++ src/test/java/Pages/SearchArtikelPage.java
@@ -11,7 +11,8 @@ public class SearchArtikelPage {
         public SearchArtikelPage(Page page){
            this.page=page;
            this.cookies=page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Jag accepterar alla cookies"));
-           this.search=page.getByRole(AriaRole.SEARCHBOX, new Page.GetByRoleOptions().setName("Sök"));
+           //this.search=page.getByRole(AriaRole.SEARCHBOX, new Page.GetByRoleOptions().setName("Sök"));
+            this.search=page.locator("#everything-sticky").getByPlaceholder("Hej! Sök med artikelnummer, registreringsnummer eller fritext.");
         }
         public void searchArtikel(String searchItem){
                 page.navigate("https://www.biltema.se/");
